ff2356eb55114bbe2ec2ee375ae9ba4318ed649c
s-aga-r <sagarsharma.s312@gmail.com>
2024-11-07 11:18:42 +0530

    ci: migrate from `black` to `ruff`

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,37 +5,43 @@ import frappeui from 'frappe-ui/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    frappeui(),
-    vue({
-      script: {
-        defineModel: true,
-        propsDestructure: true,
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    outDir: `../mail/public/frontend`,
-    emptyOutDir: true,
-    commonjsOptions: {
-      include: [/tailwind.config.js/, /node_modules/],
-    },
-    sourcemap: true,
-    target: 'es2015',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'frappe-ui': ['frappe-ui'],
-        },
-      },
-    },
-  },
-  optimizeDeps: {
-    include: ['frappe-ui > feather-icons', 'showdown', 'engine.io-client', 'prosemirror-state', 'prosemirror-view'],
-  },
+	plugins: [
+		frappeui(),
+		vue({
+			script: {
+				defineModel: true,
+				propsDestructure: true,
+			},
+		}),
+	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
+	build: {
+		outDir: `../mail/public/frontend`,
+		emptyOutDir: true,
+		commonjsOptions: {
+			include: [/tailwind.config.js/, /node_modules/],
+		},
+		sourcemap: true,
+		target: 'es2015',
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					'frappe-ui': ['frappe-ui'],
+				},
+			},
+		},
+	},
+	optimizeDeps: {
+		include: [
+			'frappe-ui > feather-icons',
+			'showdown',
+			'engine.io-client',
+			'prosemirror-state',
+			'prosemirror-view',
+		],
+	},
 })
