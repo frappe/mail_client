e4ce495e091c58fb7411b4101359784ee4038223
Akash  Tom <akash.akasht.tom@gmail.com>
2025-02-12 18:01:52 +0530

    chore(eslint): add import sorting (#102)
    * chore: add eslint-plugin-import
    
    * chore: add sorting rules
    
    * chore: add ts resolver
    
    * chore: check for unresolved imports
    
    * chore: fix match pattern
    
    * chore: sort imports
    
    * chore: ignore import/no-unresolved for socketio_port

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,30 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import pluginVue from 'eslint-plugin-vue'
+import eslint from '@eslint/js'
 import prettier from 'eslint-config-prettier'
-import pluginPrettier from 'eslint-plugin-prettier'
+import importPlugin from 'eslint-plugin-import'
+import prettierPlugin from 'eslint-plugin-prettier'
+import vuePlugin from 'eslint-plugin-vue'
+import globals from 'globals'
+import { config, configs } from 'typescript-eslint'
 
-export default tseslint.config(
-  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
+export default config(
+  {
+    files: ['**/*.{js,mjs,cjs,ts,vue}'],
+    extends: [
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
+    ],
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './frontend/tsconfig.json',
+        },
+        alias: {
+          map: ['@', './frontend/src'],
+          extensions: ['.js', '.ts', '.vue'],
+        },
+      },
+    },
+  },
   {
     languageOptions: {
       globals: {
@@ -112,17 +130,68 @@ export default tseslint.config(
       sourceType: 'module',
     },
   },
-  pluginJs.configs.recommended,
-  tseslint.configs.recommended,
-  ...pluginVue.configs['flat/recommended'],
+  eslint.configs.recommended,
+  configs.recommended,
+  ...vuePlugin.configs['flat/recommended'],
   prettier,
   {
-    plugins: { prettier: pluginPrettier },
+    plugins: { prettier: prettierPlugin },
     rules: {
       'prettier/prettier': 'error',
       'space-unary-ops': ['error', { words: true }],
       'prefer-const': 'error',
       'no-console': 'warn',
+      'import/no-duplicates': 'error',
+      'import/newline-after-import': 'error',
+      'import/order': [
+        'error',
+        {
+          alphabetize: {
+            order: 'asc',
+          },
+          named: true,
+          pathGroups: [
+            {
+              pattern: 'vue**',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: 'frappe-ui',
+              group: 'external',
+              position: 'after',
+            },
+            {
+              pattern: '@/utils/**',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@/stores/**',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@/pages/**',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@/components/**',
+              group: 'internal',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: [],
+          distinctGroup: false,
+          'newlines-between': 'always',
+        },
+      ],
+    },
+  },
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: 'off',
     },
   },
 )
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import path from 'path'
+
+import vue from '@vitejs/plugin-vue'
 import frappeui from 'frappe-ui/vite'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
